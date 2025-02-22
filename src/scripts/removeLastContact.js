36b7696503bda4fe1d34b01cc3cdc27bcf5bcb89
Svetakler <svetakler86@gmail.com>
2025-02-22 22:01:32 +0200

    remove last contact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,9 +1,26 @@
 import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-const removeAllContacts = async () => {
-  await fs.writeFile(PATH_DB, JSON.stringify([], null, 2));
-  console.log('Last contact was deleted');
+const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    if (contacts.length === 0) {
+      console.log('No contacts to delete.');
+      return;
+    }
+
+    const updatedContacts = contacts.filter(
+      (_, index) => index !== contacts.length - 1,
+    );
+
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+
+    console.log('Last contact has been deleted.');
+  } catch (error) {
+    console.error('Error deleting the last contact:', error);
+  }
 };
 
-removeAllContacts();
+removeLastContact();
